02336497a734f6d14924cd45cf82e44202cb544a
Atif Rao <atoohr8452@gmail.com>
2024-03-27 03:39:00 +0500

    second commit

---
+++ index.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter first number", type: "number", name: "firstnumber" },
+    { message: "enter second number", type: "number", name: "secondnumber" },
+    {
+        meassge: "select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "substration", "multiaplication", "division"],
+    },
+]);
+console.log(answer);
+//conditional statement
+if (answer.operator === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "substration") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "multiaplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("please select valid operator");
+}
